3878f5dd77c3d58e8a46fb208c95e8327cc27110
Carlos Vital <carloshenrique.vital@gmail.com>
2023-09-17 03:12:28 +0000

    improve construction page style

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
+import styles from "./home.module.css";
+
 function Home() {
-  return <h1>Em construção</h1>;
+  return <h1 className={styles.in_constructuion_text}>Em construção</h1>;
 }
 
 export default Home;
