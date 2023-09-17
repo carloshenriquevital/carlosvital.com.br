f58a571f8d3f4c5ff444ad5d511939ed14b7d919
Carlos Vital <carloshenrique.vital@gmail.com>
2023-09-17 03:16:09 +0000

    fix css class name

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import styles from "./home.module.css";
 
 function Home() {
-  return <h1 className={styles.in_constructuion_text}>Em construção</h1>;
+  return <h1 className={styles.in_construction_text}>Em construção</h1>;
 }
 
 export default Home;
